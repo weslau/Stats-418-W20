257e90c639cebff43169d6b255316377bc6dbf67
Wesley Lau <wlau218@g.ucla.edu>
2020-01-17 20:11:27 -0800

    application.py works locally, flask thing deployed to centralus

--- hw1/q2/flask_project/application.py
+++ hw1/q2/flask_project/application.py
@@ -1,21 +1,41 @@
 from flask import Flask
 import logging
+import datetime
+
 app = Flask(__name__)
 
-logging.basicConfig(filename='demo.log', level=logging.DEBUG)
-@app.route("/")
-def hello():
-    return "Hello World! Good "
+##logging.basicConfig(filename='demo.log', filemode='w', level=logging.DEBUG)
 
-##get the time from logging info, it will be in UTC time tho
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%H:%M:%S')
+logging.warning('is when this event was logged.')
+
+
+##logging.warning('Hello World! Good %s')
+##get the current time in military time
+z = int(datetime.datetime.today().strftime('%H')) ##z is just the number for hour
+##logging.warning(z)
+##convert the military time into morning, afternoon, etc
+
+##function to convert hours to time of day
 def gettime(curr_time):
-    if (curr_time<6 and curr_time>21):
-        return "night"
-    elif curr_time<12:
+    if (curr_time>=6 and curr_time<12):
         return "morning"
-    elif curr_time<18:
+    elif (curr_time>=12 and curr_time<18):
         return "afternoon"
-    elif curr_time<21:
+    elif (curr_time>=18 and curr_time<21):
         return "evening"
+    elif (curr_time>=21 and curr_time<=24) or (curr_time>=0 and curr_time<6):
+        return "night"
     else:
         return "unrecognized time"
+
+time_day = gettime(z) ##run function on the current hour
+##logging.warning(time_day)
+
+@app.route("/")
+def hello():
+    return "Hello World! Good " + time_day
+
+
+
+##logging.shutdown()
